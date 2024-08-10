1310d2df0956c27f6109ef8ece016f8f5e8550b4
zyle-geralde <zylgerdlp19@gmail.com>
2024-08-10 17:48:26 +0800

    Log In Container

--- src/components/LogInSignUpComponents/LogInCont.jsx
+++ src/components/LogInSignUpComponents/LogInCont.jsx
@@ -2,21 +2,22 @@ import "../../styles/logInContstyle.css"
 
 function LogInCont() {
     return (
-        <div class="contLog">
-            <h1 class="webTitle">PulseNet</h1>
-            <div class="inputGroup">
-                <div class="form-floating mb-3">
-                    <input type="text" class="form-control" id="floatingInput" placeholder="xxx"></input>
-                    <label for="floatingInput">Username</label>
+        <div className="contLog">
+            <h1 className="webTitle"><span className = "pulseText">Pulse</span>Net</h1>
+            <div className="inputGroup">
+                <div className="form-floating mb-3">
+                    <input type="text" className="form-control" id="floatingInput" placeholder="xxx"></input>
+                    <label htmlFor="floatingInput">Username</label>
                 </div>
-                <div class="form-floating mb-3">
-                    <input type="password" class="form-control passwordInput" id="floatingInput2" placeholder="xxx"></input>
-                    <label for="floatingInput2">Password</label>
+                <div className="form-floating mb-3">
+                    <input type="password" className="form-control passwordInput" id="floatingInput2" placeholder="xxx"></input>
+                    <label htmlFor="floatingInput2">Password</label>
                 </div>
             </div>
-            <div class = "logButCont">
-                <button class = "btn btn-danger logBut">Log In</button>
+            <div className = "logButCont">
+                <button className = "btn btn-danger logBut">Log In</button>
             </div>
+            <p className = "signMessage">Don't have an account? <span><a href="./signUp" className = "signLink">Sign Up</a></span></p>
         </div>
     )
 }
