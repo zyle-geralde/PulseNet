9b9a50c74979f1426ccdfa6fe00682af164eeff6
zyle-geralde <zylgerdlp19@gmail.com>
2024-08-12 17:09:41 +0800

    signInContainer

---
+++ src/components/LogInSignUpComponents/SignInCon.jsx
@@ -0,0 +1,24 @@
+import "../../styles/logInContstyle.css"
+
+function SignInCont(){
+    return(
+        <div className="contLog">
+            <h1 className="webTitle">Register</h1>
+            <div className="inputGroup">
+                <div className="form-floating mb-3">
+                    <input type="text" className="form-control" id="floatingInput" placeholder="xxx"></input>
+                    <label htmlFor="floatingInput">Username</label>
+                </div>
+                <div className="form-floating mb-3">
+                    <input type="password" className="form-control passwordInput" id="floatingInput2" placeholder="xxx"></input>
+                    <label htmlFor="floatingInput2">Password</label>
+                </div>
+            </div>
+            <div className = "logButCont">
+                <button className = "btn btn-danger logBut">Log In</button>
+            </div>
+            <p className = "signMessage">Don't have an account? <span><a href="./signUp" className = "signLink">Sign Up</a></span></p>
+        </div>
+    )
+}
+export default SignInCont
