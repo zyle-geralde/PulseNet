de7b30d8577fc5ef7401d55496ec9060553adfe6
zyle-geralde <zylgerdlp19@gmail.com>
2024-08-12 15:57:50 +0800

    added routing

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import LogInCont from './components/LogInSignUpComponents/LogInCont'
-import LogInImageCont from './components/LogInSignUpComponents/LogInImageCont'
+import { Routes, Route } from 'react-router-dom';
+import LogMain from './components/LogInSignUpComponents/LogInMainCont'
 
 
 function App() {
-    return(
-        <>
-            <LogInImageCont></LogInImageCont>
-            <LogInCont></LogInCont>
-        </>
+    return (
+        <Routes>
+            <Route path='/' element = {LogMain()}/>
+            <Route path='/LogIn' element = {LogMain()}/>
+        </Routes>
     )
 }
 
---
+++ src/components/LogInSignUpComponents/LogInMainCont.jsx
@@ -0,0 +1,14 @@
+import LogInCont from "./LogInCont"
+import LogInImageCont from "./LogInImageCont"
+
+
+function LogMain(){
+    return(
+    <>
+        <LogInImageCont />
+        <LogInCont />
+    </>
+    )
+}
+
+export default LogMain
