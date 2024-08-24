cdcdce3ddc96a098965e95b014b1fd609cfa6537
zyle-geralde <zylgerdlp19@gmail.com>
2024-08-24 12:40:35 +0800

    auth

--- PulseNet/src/App.jsx
+++ PulseNet/src/App.jsx
@@ -5,6 +5,7 @@ import './App.css'
 import { Routes, Route } from 'react-router-dom';
 import LogMain from './components/LogInSignUpComponents/LogInMainCont'
 import SignMainCont from './components/LogInSignUpComponents/SignInMainCont';
+import showWelcome from './components/mainPageComponents/trypage';
 
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
             <Route path='/' element = {LogMain()}/>
             <Route path='/LogIn' element = {LogMain()}/>
             <Route path='/signUp' element = {SignMainCont()}/>
+            <Route path='/welcome' element = {showWelcome()}/>
         </Routes>
     )
 }
---
+++ PulseNet/src/components/mainPageComponents/trypage.jsx
@@ -0,0 +1,16 @@
+import axios from "axios";
+import { Fragment, useState } from "react";
+
+
+function showWelcome() {
+
+    return (
+        <>
+            <h1>
+                Welcome
+            </h1>
+        </>
+    )
+}
+
+export default showWelcome
