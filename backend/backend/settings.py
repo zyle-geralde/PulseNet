f9f8bec6d013b455ebbe7a43ecec9b2187a557a0
zyle-geralde <zylgerdlp19@gmail.com>
2024-10-21 18:03:39 +0800

    added corsheaders to installed apps

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -57,7 +57,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'api',
-    'rest_framework'
+    'rest_framework',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
