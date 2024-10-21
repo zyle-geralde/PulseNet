d2fee2ae9a489043043d74d16dd64ccbb2716a60
zyle-geralde <zylgerdlp19@gmail.com>
2024-10-21 17:54:43 +0800

    loadenv called

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -16,6 +16,8 @@ from dotenv import load_dotenv
 
 import os
 
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
