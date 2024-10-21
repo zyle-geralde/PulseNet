1579d2b0c86d5bbe13599d412cba0a366685137e
zyle-geralde <zylgerdlp19@gmail.com>
2024-10-21 18:22:32 +0800

    UserSerializer add

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,2 +1,10 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
+
+#serialize data to pass from backend to frontend and vice versa
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User# model that you want to serialize
+        fields = ["id","username","password"] # fields that you want to serialize
+
