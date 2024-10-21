59c8d6ba3437029ce1a944120ece48f7a8f5d9d0
zyle-geralde <zylgerdlp19@gmail.com>
2024-10-21 17:53:48 +0800

    importing libraries

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from datetime import timedelta
+from dotenv import load_dotenv
+
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
