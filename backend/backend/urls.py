97be897372ced3246ed111bd40420581d46ae138
zyle-geralde <zylgerdlp19@gmail.com>
2024-10-21 19:07:42 +0800

    importing create user view in backend urls

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from api.views import CreateUserView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
