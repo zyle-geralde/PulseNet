3ea12f67c5da579ccefbdd3b9b5c24e26b4c3261
zyle-geralde <zylgerdlp19@gmail.com>
2024-10-21 17:56:28 +0800

    rest framework setting added

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -33,6 +33,15 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ),
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated",
+    ],
+}
+
 
 # Application definition
 
