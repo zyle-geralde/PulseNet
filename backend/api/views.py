bf82fb3768d17d1d24acb2cb9988b86e8eb09991
zyle-geralde <zylgerdlp19@gmail.com>
2024-10-21 19:05:12 +0800

    added create user view

--- backend/api/views.py
+++ backend/api/views.py
@@ -4,3 +4,9 @@ from rest_framework import generics
 from .serializers import UserSerializer
 from rest_framework.permissions import IsAuthenticated, AllowAny
 # Create your views here.
+
+
+class CreateUserView(generics.CreateAPIView):
+    queryset = User.objects.all() #get all Users
+    serializer_class = UserSerializer #call the serializer to identify which fields to use
+    permission_classes = [AllowAny] #allow anyone to access since even if they are not authenticated
