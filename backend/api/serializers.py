747253e99540f2854f6573d12ee58e7715f54d20
zyle-geralde <zylgerdlp19@gmail.com>
2024-10-21 19:44:33 +0800

    completed api service

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
 from rest_framework.validators import UniqueValidator
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework.exceptions import AuthenticationFailed
 
 #serialize data to pass from backend to frontend and vice versa
 
@@ -11,10 +13,39 @@ class UserSerializer(serializers.ModelSerializer):
     )
     class Meta:
         model = User# model that you want to serialize
-        fields = ["id","email","username","password"] # fields that you want to serialize
+        fields = ["id","email","password"] # fields that you want to serialize
         extra_kwargs = {"password":{"write_only":True}} #accept password when creating a user but it will not be returned when giving information to the user.
 
     def create(self,validated_data):
-        user = User.objects.create_user(**validated_data)
+        #user = User.objects.create_user(**validated_data)
+        email = validated_data['email']
+        password = validated_data.pop('password')
+        user = User(username=email, email=email)  # Set email as username
+        user.set_password(password)  # Hash the password
+        user.save()
+
         return user
+    
+
+
+class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
+    def validate(self, attrs):
+        # Use email to fetch the user instead of username
+        email = attrs.get("username")
+        password = attrs.get("password")
+
+        try:
+            user = User.objects.get(email=email)
+        except User.DoesNotExist:
+            raise AuthenticationFailed("No user with this email found.")
+
+        # Check if the password is correct
+        if not user.check_password(password):
+            raise AuthenticationFailed("Incorrect password.")
+
+        # Proceed with the normal token creation if valid
+        return super().validate({
+            "username": user.username,  # Token still requires username internally
+            "password": password,
+        })
 
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -16,13 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-from api.views import CreateUserView
+from api.views import CreateUserView,CustomTokenObtainPairView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/user/register/", CreateUserView.as_view(), name="register"),
-    path("api/token/", TokenObtainPairView.as_view(), name="get_token"),
+    path("api/token/", CustomTokenObtainPairView.as_view(), name="get_token"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="refresh"),
     path("api-auth/", include("rest_framework.urls")),
 ]
