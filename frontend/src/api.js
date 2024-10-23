55fe2dc66f3b3e2faa0dd98f839ab3ce9de2c736
zyle-geralde <zylgerdlp19@gmail.com>
2024-10-24 04:35:54 +0800

    api.js added comments

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,14 +2,15 @@ import axios from "axios";
 import { ACCESS_TOKEN } from "./constants";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL
+  baseURL: import.meta.env.VITE_API_URL //allows us to use the VITE_API_URL in the .env as our base URL
 });
+
 
 api.interceptors.request.use(
   (config) => {
-    const token = localStorage.getItem(ACCESS_TOKEN);
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
+    const token = localStorage.getItem(ACCESS_TOKEN);// gets access token in localStorage
+    if (token) { // checks if token is available
+      config.headers.Authorization = `Bearer ${token}`;// updates the Authorization header with the token to be used in authentication by django
     }
     return config;
   },
