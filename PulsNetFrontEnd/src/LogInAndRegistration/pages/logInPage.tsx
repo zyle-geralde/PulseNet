ba29ca36a81c5be7eb1709d9744d07a6c6d4ff03
zyle-geralde <zylgerdlp19@gmail.com>
2024-11-16 19:07:55 +0800

    finished log in interface

--- PulsNetFrontEnd/src/LogInAndRegistration/pages/logInPage.tsx
+++ PulsNetFrontEnd/src/LogInAndRegistration/pages/logInPage.tsx
@@ -4,14 +4,28 @@ import InputForm from "../components/Inputform";
 import TextLink from "../components/textLink";
 
 function LogInPage() {
+
     return (
         <>
-            <HeadComp heading="LOG IN" />
-            <div className="formDiv">
-                <InputForm typePass="email" idPass="floatingInput" placeholderPass="xxx@xxx.com" forContent="Email address" />
-                <InputForm typePass="password" idPass="floatingPassword" placeholderPass="Password" forContent="Password" />
-                <TextLink linkPass="" textPass="Don't have an account? Sign Up now" classPass="signUpLink"/>
-                <ButtonComp textPass="Log In" classPass="logInButton"/>
+            <div style={{
+
+             }} className="LogInImage">
+                
+                <img src="./images/social-media-5187243_1920.png" style={{
+                    width: "100%", borderRadius: "10px",
+                    boxShadow: "10px 10px 10px rgb(225, 225, 225)",
+                    marginTop: "30px",
+                }}></img>
+            </div>
+            <div style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center" }}>
+                <div style={{ fontSize: "50px", fontFamily: "'Pacifico', serif", color: "rgb(238, 27, 91)", textAlign: "center", marginRight:"10px"}}>PULSENET</div>
+                <HeadComp heading="LOG IN" />
+                <div className="formDiv">
+                    <InputForm typePass="email" idPass="floatingInput" placeholderPass="xxx@xxx.com" forContent="Email address" />
+                    <InputForm typePass="password" idPass="floatingPassword" placeholderPass="Password" forContent="Password" />
+                    <TextLink linkPass="" textPass="Don't have an account? Sign Up now" classPass="signUpLink" />
+                    <ButtonComp textPass="Log In" classPass="logInButton" />
+                </div>
             </div>
         </>
     )
