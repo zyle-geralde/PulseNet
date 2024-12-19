9b180887e4f74de29a988ebb50d54b789bc4571e
zyle-geralde <zylgerdlp19@gmail.com>
2024-12-19 15:48:25 +0800

    added search

--- PulsNetFrontEnd/src/HomePage/pages/AllPost.tsx
+++ PulsNetFrontEnd/src/HomePage/pages/AllPost.tsx
@@ -8,7 +8,16 @@ function AllPost() {
         {/* header component */}
         <div className="headNav">
             <div className="Logo">
-                <div style={{ fontSize: "30px", fontFamily: "'Pacifico', serif", color: "rgb(238, 27, 91)", textAlign: "center", marginLeft:"30px"}}>PULSENET</div>
+                <div style={{ fontSize: "30px", fontFamily: "'Pacifico', serif", color: "rgb(238, 27, 91)", textAlign: "center", marginLeft:"30px"}}>Pnet</div>
+            </div>
+            <div className="iconList">
+                <img src="/images/home.png" className="homeImg"></img>
+                <img src="/images/user.png" className="userImg"></img>
+                <img src="/images/friends.png" className="friendImg"></img>
+            </div>
+            <div className="searchPeople">
+                <input type="text" placeholder="search" className="searchInp"></input>
+                <img src="/images/searchlogo.png" className="searchImg"></img>
             </div>
         </div>
     </div>
