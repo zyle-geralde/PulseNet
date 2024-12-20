54e04ccb7b30f6d4dee8da5fea1b98c51f460e47
zyle-geralde <zylgerdlp19@gmail.com>
2024-12-20 09:27:51 +0800

    modified imageurl

--- PulsNetFrontEnd/src/HomePage/pages/AllPost.tsx
+++ PulsNetFrontEnd/src/HomePage/pages/AllPost.tsx
@@ -43,6 +43,70 @@ function AllPost() {
             </div>
         </div>
 
+        <div className="allPostCont">
+            <div className="allProfCont">
+                <img src="images/userhold.png" className="userProfhold"></img>
+                <div className="namePost">
+                    <div className="username">
+                        thomas doe eddison saldnfjk
+                    </div>
+                    <div className="postdate">
+                        July 10, 2023 jasdnfkjsnafkjnsdakj
+                    </div>
+                </div>
+            </div>
+            <div className="postContext">
+                kjsadnfkjsanfkjsndfjknsadkjfnsakjfnsakjnfjaskdnfkjnasfdkjnsakdjfnaskjdfnksajndfkjsandfkjnsafkjansdjkfnasdkjfn
+            </div>
+            <div className="postImgCont">
+                <img src="images/social-media-5187243_1920.png" className="ImgHolder"></img>
+            </div>
+            <div className="likeComment">
+                <div className="likeContclass">
+                    <div className="likeimgcont">
+                        <img src="images/thumbsUp.png" className="thumbsUpcls"></img>
+                    </div>
+                    <div className="countLike">2</div>
+                </div>
+                <div className="commentContclass">
+                    <img src="images/commentIcon.png" className="commentcls"></img>
+                    <div className="commentlabel" data-bs-toggle="modal" data-bs-target="#exampleModal">Comment</div>
+                </div>
+            </div>
+        </div>
+
+        <div className="allPostCont">
+            <div className="allProfCont">
+                <img src="images/userhold.png" className="userProfhold"></img>
+                <div className="namePost">
+                    <div className="username">
+                        thomas doe eddison saldnfjk
+                    </div>
+                    <div className="postdate">
+                        July 10, 2023 jasdnfkjsnafkjnsdakj
+                    </div>
+                </div>
+            </div>
+            <div className="postContext">
+                kjsadnfkjsanfkjsndfjknsadkjfnsakjfnsakjnfjaskdnfkjnasfdkjnsakdjfnaskjdfnksajndfkjsandfkjnsafkjansdjkfnasdkjfn
+            </div>
+            <div className="postImgCont">
+                <img src="images/social-media-5187243_1920.png" className="ImgHolder"></img>
+            </div>
+            <div className="likeComment">
+                <div className="likeContclass">
+                    <div className="likeimgcont">
+                        <img src="images/thumbsUp.png" className="thumbsUpcls"></img>
+                    </div>
+                    <div className="countLike">2</div>
+                </div>
+                <div className="commentContclass">
+                    <img src="images/commentIcon.png" className="commentcls"></img>
+                    <div className="commentlabel" data-bs-toggle="modal" data-bs-target="#exampleModal">Comment</div>
+                </div>
+            </div>
+        </div>
+
         <div className="modal fade" id="exampleModal" aria-labelledby="exampleModalLabel" aria-hidden="true">
             <div className="modal-dialog">
                 <div className="modal-content">
@@ -55,57 +119,27 @@ function AllPost() {
                             <div className="IndivComment">
                                 <img src="images/userhold.png" className="commentUserimg"></img>
                                 <div className="usercommentTextCont">
-                                    <div className="userCommentName">
-                                        john does asdnfslndfjsadnasdfasdfsafsdfaasdfasfsafasdfsadfasfsafsadf
+                                    <div className="uppercomment">
+                                        <div className="userCommentName">
+                                            john does asdnfslndfjsadnasdfasdfsafsdfaasdfasfsafasdfsadfasfsafsadf
+                                        </div>
+                                        <div className="userCommentdiv">
+                                            sakjdfjkasdfkjasdfjkasdfkjsadkjfnsadjkfnaskjdfnjkasdnfkjdasnfkjsafdn
+                                            asjkdfnjksanfdjkanfsjkasnfkjasndfksanjkfnksjanfjasf
+                                            asdkfnsakjdfnskjadfnskjanfkjsandfkjsandkjfnsakdjfnaskjdf
+                                            sakjdfjkasdfkjasdfjkasdfkjsadkjfnsadjkfnaskjdfnjkasdnfkjdasnfkjsafdn
+                                            asjkdfnjksanfdjkanfsjkasnfkjasndfksanjkfnksjanfjasf
+                                            asdkfnsakjdfnskjadfnskjanfkjsandfkjsandkjfnsakdjfnaskjdf
+                                        </div>
                                     </div>
-                                    <div className="userCommentdiv">
-                                        sakjdfjkasdfkjasdfjkasdfkjsadkjfnsadjkfnaskjdfnjkasdnfkjdasnfkjsafdn
-                                        asjkdfnjksanfdjkanfsjkasnfkjasndfksanjkfnksjanfjasf
-                                        asdkfnsakjdfnskjadfnskjanfkjsandfkjsandkjfnsakdjfnaskjdf
-                                        sakjdfjkasdfkjasdfjkasdfkjsadkjfnsadjkfnaskjdfnjkasdnfkjdasnfkjsafdn
-                                        asjkdfnjksanfdjkanfsjkasnfkjasndfksanjkfnksjanfjasf
-                                        asdkfnsakjdfnskjadfnskjanfkjsandfkjsandkjfnsakdjfnaskjdf
-                                    </div>
+                                    <div className="dateCommented">July 6 2023</div>
                                 </div>
                             </div>
 
-                            <div className="IndivComment">
-                                <img src="images/userhold.png" className="commentUserimg"></img>
-                                <div className="usercommentTextCont">
-                                    <div className="userCommentName">
-                                        john does asdnfslndfjsadnasdfasdfsafsdfaasdfasfsafasdfsadfasfsafsadf
-                                    </div>
-                                    <div className="userCommentdiv">
-                                        sakjdfjkasdfkjasdfjkasdfkjsadkjfnsadjkfnaskjdfnjkasdnfkjdasnfkjsafdn
-                                        asjkdfnjksanfdjkanfsjkasnfkjasndfksanjkfnksjanfjasf
-                                        asdkfnsakjdfnskjadfnskjanfkjsandfkjsandkjfnsakdjfnaskjdf
-                                        sakjdfjkasdfkjasdfjkasdfkjsadkjfnsadjkfnaskjdfnjkasdnfkjdasnfkjsafdn
-                                        asjkdfnjksanfdjkanfsjkasnfkjasndfksanjkfnksjanfjasf
-                                        asdkfnsakjdfnskjadfnskjanfkjsandfkjsandkjfnsakdjfnaskjdf
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div className="IndivComment">
-                                <img src="images/userhold.png" className="commentUserimg"></img>
-                                <div className="usercommentTextCont">
-                                    <div className="userCommentName">
-                                        john does asdnfslndfjsadnasdfasdfsafsdfaasdfasfsafasdfsadfasfsafsadf
-                                    </div>
-                                    <div className="userCommentdiv">
-                                        sakjdfjkasdfkjasdfjkasdfkjsadkjfnsadjkfnaskjdfnjkasdnfkjdasnfkjsafdn
-                                        asjkdfnjksanfdjkanfsjkasnfkjasndfksanjkfnksjanfjasf
-                                        asdkfnsakjdfnskjadfnskjanfkjsandfkjsandkjfnsakdjfnaskjdf
-                                        sakjdfjkasdfkjasdfjkasdfkjsadkjfnsadjkfnaskjdfnjkasdnfkjdasnfkjsafdn
-                                        asjkdfnjksanfdjkanfsjkasnfkjasndfksanjkfnksjanfjasf
-                                        asdkfnsakjdfnskjadfnskjanfkjsandfkjsandkjfnsakdjfnaskjdf
-                                    </div>
-                                </div>
-                            </div>
                         </div>
                         <div className="commentsendCont">
                             <input type="text" placeholder="type here " className="commentInp"></input>
-                            <img src = "images/sendPost.png" className="commentSend"></img>
+                            <img src="images/sendPost.png" className="commentSend"></img>
                         </div>
                     </div>
                 </div>
--- backend/PulseNetBackend/LogInRegistration/models.py
+++ backend/PulseNetBackend/LogInRegistration/models.py
@@ -3,10 +3,11 @@ from django.db import models
 # Create your models here.
 
 class CustomerUser(models.Model):
-    firstname = models.CharField(max_length=255)
-    lastname = models.CharField(max_length=255)
-    age = models.CharField(max_length=255)
-    gender = models.CharField(max_length=255)
-    address = models.CharField(max_length=255)
-    email = models.CharField(max_length=255)
-    password = models.CharField(max_length=1000)
+    firstname = models.CharField(max_length=255,default="none")
+    lastname = models.CharField(max_length=255,default="none")
+    age = models.CharField(max_length=255,default="0")
+    gender = models.CharField(max_length=255,default="none")
+    address = models.CharField(max_length=255,default="none")
+    email = models.CharField(max_length=255,default="none")
+    password = models.CharField(max_length=1000,default="none")
+    imageurl = models.CharField(max_length=1000,default="images/userhold.png")
