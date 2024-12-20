bac8e1987e9b7e3c33cb0a97aa1f152823dbb125
zyle-geralde <zylgerdlp19@gmail.com>
2024-12-20 09:51:21 +0800

    post model added

--- backend/PulseNetBackend/LogInRegistration/models.py
+++ backend/PulseNetBackend/LogInRegistration/models.py
@@ -11,3 +11,15 @@ class CustomerUser(models.Model):
     email = models.CharField(max_length=255,default="none")
     password = models.CharField(max_length=1000,default="none")
     imageurl = models.CharField(max_length=1000,default="images/userhold.png")
+
+
+class Post(models.Model):
+    userId = models.ForeignKey(CustomerUser,on_delete=models.CASCADE)
+    caption = models.TextField(default = "This is a default text")
+    imageUrl = models.CharField(max_length=1000,default="images/userhold.png")
+    dateUpdated = models.DateTimeField(auto_now=True)
+    likes = models.ManyToManyField(CustomerUser,related_name="customer_likes")
+
+
+    def __str__(self):
+        return f"{self.imageUrl} by {self.dateUpdated}"
