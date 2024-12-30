2fe6ac16ba8c0d500e85da83eb85def65c1935a9
zyle-geralde <zylgerdlp19@gmail.com>
2024-12-30 14:08:29 +0800

    finsihed edit react ui

--- PulsNetFrontEnd/src/HomePage/components/profileComp.tsx
+++ PulsNetFrontEnd/src/HomePage/components/profileComp.tsx
@@ -11,7 +11,14 @@ function ProfileComp() {
         "age":"",
         "gender":"",
         "address":"",
-        "imageurl":"",})
+        "imageurl": "",
+    })
+    
+    var [forfirstname, setforfirstname] = useState("")
+    var [forlastname, setforlastname] = useState("")
+    var [forgender, setforgender] = useState("")
+    var [foraddress, setforaddress] = useState("")
+    var [forage, setforage] = useState("")
 
     useEffect(() => {
         GetLoggedUser(setprofileInfo)
@@ -65,7 +72,65 @@ function ProfileComp() {
                 <strong>Address: </strong><span>{profileInfo.address}</span>
         </div>
         </div>
-        <div className="AddPhotoEdit" style={{"width":"50px"}}>Edit</div>
+        <div className="AddPhotoEdit" style={{ "width": "50px" }} data-bs-toggle="modal" data-bs-target="#editProfileModal" onClick={function (e) {
+            setforfirstname(profileInfo.firstname)
+            setforlastname(profileInfo.lastname)
+            setforage(profileInfo.age)
+            setforgender(profileInfo.gender)
+            setforaddress(profileInfo.address)
+        }}>Edit</div>
+        
+
+        <div className="modal fade" id="editProfileModal" aria-labelledby="exampleCommentModalLabel" aria-hidden="true">
+            <div className="modal-dialog">
+                <div className="modal-content">
+                    <div className="modal-header">
+                        <h1 className="modal-title fs-5" id="exampleCommentModalLabel">Edit Profile</h1>
+                        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close" onClick={
+                            function (e: React.MouseEvent<HTMLButtonElement>) {
+                            }}></button>
+                    </div>
+                    <div className="modal-body">
+                        <h6>First Name</h6>
+                        <input type="text" style={{ "width": "100%", "borderStyle": "solid", "borderWidth": "1.5px", "borderColor": "lightgray", "padding": "10px", "borderRadius": "5px", "marginBottom": "10px" }}
+                            value={forfirstname} onChange={function (e) {
+                            setforfirstname(e.target.value)
+                        }}></input>
+                        <h6>Last Name</h6>
+                        <input type="text" style={{ "width": "100%", "borderStyle": "solid", "borderWidth": "1.5px", "borderColor": "lightgray", "padding": "10px", "borderRadius": "5px", "marginBottom": "10px" }}
+                            value={forlastname} onChange={function (e) {
+                            setforlastname(e.target.value)
+                        }}></input>
+                        <h6>Age</h6>
+                        <input type="text" style={{ "width": "100%", "borderStyle": "solid", "borderWidth": "1.5px", "borderColor": "lightgray", "padding": "10px", "borderRadius": "5px", "marginBottom": "10px" }}
+                            value={forage} onChange={function (e) {
+                            setforage(e.target.value)
+                        }}></input>
+                        <h6>Gender</h6>
+                        <input type="text" style={{ "width": "100%", "borderStyle": "solid", "borderWidth": "1.5px", "borderColor": "lightgray", "padding": "10px", "borderRadius": "5px", "marginBottom": "10px" }}
+                            value={forgender} onChange={function (e) {
+                            setforgender(e.target.value)
+                        }}></input>
+                        <h6>Address</h6>
+                        <input type="text" style={{ "width": "100%", "borderStyle": "solid", "borderWidth": "1.5px", "borderColor": "lightgray", "padding": "10px", "borderRadius": "5px", "marginBottom": "10px" }}
+                            value={foraddress} onChange={function (e) {
+                            setforaddress(e.target.value)
+                        }}></input>
+                        <div style={{ "display": "flex", "flexDirection": "row", "justifyContent": "end", "alignItems": "center" }}>
+                            <img src="images/sendPost.png" className="sendPost" onClick={function (e) {
+                                if (forfirstname.trim() == "" || forlastname.trim() == "" || forage.trim() == "" || forgender.trim() == "" ||
+                                    foraddress.trim() == "") {
+                                    alert("bad")
+                                }
+                                else {
+                                    alert("good")
+                                }
+                            }}></img>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
 </div>
 }
 
